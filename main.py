6e7db4ea13fcd7d4e8ecfd4dca5e7c739c64ff46
pslcorrea <pslcorrea@gmail.com>
2023-09-13 12:41:38 -0300

    Finalização de layout

--- main.py
+++ main.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route("/galeria")
+def galeria():
+   return render_template('galeria.html')
+
+@app.route("/doacoes")
+def doacores():
+   return render_template('doacoes.html')
+
 if __name__== "__main__":
   app.run(debug=True)
